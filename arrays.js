379f08d55baf1ef9db7e03dece8db37265769d34
Jackson Bates <jkbates222@gmail.com>
2018-09-11 02:00:38 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var array = []
+function addElementToBeginningOfArray(array, element) {
+  var thing = [element, ...array]
+  return thing
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+  return array.unshift(element)
+}
